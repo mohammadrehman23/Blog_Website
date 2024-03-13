7947525896fb992fed61fa14a5d716d0dd4491dc
mohammadrehman23 <161475445+mohammadrehman23@users.noreply.github.com>
2024-03-13 16:03:51 +0530

    Add files via upload

---
+++ server/routes/main.js
@@ -0,0 +1,86 @@
+const express=require('express');
+const router=express.Router();
+const Post=require('../models/Post');
+
+router.get('',async(req,res)=>{
+    try{
+        const locals={
+            title:"Blog",
+            description:"Simple blog"
+        }
+        let perPage=5;
+        let page=req.query.page||1;
+
+        const data=await Post.aggregate([ { $sort: { createdAt:-1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count= await Post.countDocuments();
+        const nextPage=parseInt(page) + 1;
+        const hasnextPage=nextPage <= Math.ceil(count/perPage);
+
+        res.render('index',{
+            locals,
+            data,
+            current:page,
+            nextPage:hasnextPage ? nextPage:null
+            // currentRoute:'/'
+        });
+    }catch(error){
+        console.log(error);
+    }
+
+})
+    
+router.get('/post/:id',async(req,res)=>{
+    try{
+        let slug =req.params.id;
+        
+        const data=await Post.findById({ _id: slug});
+
+        const locals={
+            title:data.title,
+            description:"Body Parted"
+            // currentRoute:`/post/${slug}`
+        }
+
+        res.render('post',{locals,data});
+    }catch(error){
+        console.log(error);
+    }
+});
+
+router.post('/search',async(req,res)=>{
+    try{
+        const locals={
+            title:"Search",
+            description:"Body Parted"
+        }
+        let searchTerm =req.body.searchTerm;
+        const searchNoSpecialChar=searchTerm.replace(/{^a-zA-Z0-9}/g,"")
+
+        const data=await Post.find({
+            $or:[
+                { title: { $regex:new RegExp(searchNoSpecialChar,'i') }},
+                { body: { $regex:new RegExp(searchNoSpecialChar,'i') }}
+            ]
+        });
+        res.render("search",{
+            data,
+            locals
+        });
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
+// router.get('/about',(req,res)=>{
+//     res.render('about',{
+//         currentRoute:'/about'
+//     });
+
+// });
+
+module.exports=router;
